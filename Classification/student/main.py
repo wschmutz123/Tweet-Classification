c9c759213774109b0827894985dc7e0f0747d5c2
Will Schmutz <will.schmutz@ibm.com>
2024-10-29 12:07:08 -0700

    clean up code

--- Classification/student/main.py
+++ Classification/student/main.py
@@ -28,6 +28,9 @@ warnings.simplefilter(action="ignore", category=ConvergenceWarning)
 np.random.seed(445)
 
 def extract_word(input_string):
+    """
+    Extracts all words from a input string
+    """
     final_list = []
     for punctuation in string.punctuation:
         input_string = input_string.replace(punctuation," ")
@@ -36,6 +39,9 @@ def extract_word(input_string):
     return final_list
 
 def generate_ngrams(s, n):
+    """
+    Removed punctuations and creates a list of multiple words based on the value n
+    """
     for punctuation in string.punctuation:
         s = s.replace(punctuation," ")
     s = s.lower()
@@ -47,6 +53,9 @@ def generate_ngrams(s, n):
 
 
 def extract_dictionary(df):
+    """
+    Creates a dictionary of unique words in dataframe
+    """
     word_dict = {}
     i = 0
     counter = 0
@@ -60,12 +69,18 @@ def extract_dictionary(df):
         i = i + 1
     return word_dict
 
-def tfidif_vector(dataframe):
+def tfidif_vector(X):
+    """
+    Uses TFIDFVectorizer to create array dataframe
+    """
     from sklearn.feature_extraction.text import TfidfVectorizer
     td = TfidfVectorizer()
     X = td.fit_transform(X).toarray()
 
 def n_grams_dictionary(df):
+    """
+    Creates a dictionary of unique combinations of words in dataframe
+    """
     word_dict = {}
     i = 0
     counter = 0
@@ -82,6 +97,9 @@ def n_grams_dictionary(df):
     return word_dict
 
 def generate_n_grams_matrix(df, word_dict):
+    """
+    Creates a matrix of unique combinations of words in dataframe of length unique n_grams x numbers of tweets
+    """
     number_of_reviews = df.shape[0]
     number_of_words = len(word_dict)
     feature_matrix = np.zeros((number_of_reviews, number_of_words))
@@ -98,20 +116,9 @@ def generate_n_grams_matrix(df, word_dict):
 
 
 def generate_feature_matrix(df, word_dict):
-    number_of_reviews = df.shape[0]
-    number_of_words = len(word_dict)
-    feature_matrix = np.zeros((number_of_reviews, number_of_words))
-    row_number = 0
-    for text in df["text"]:
-        list = extract_word(text)
-        for indWord in list:
-            if indWord in word_dict:
-                feature_matrix[row_number,word_dict[indWord]] += 1
-        row_number = row_number + 1
-    return feature_matrix
-
-
-def generate_multi_class_feature_matrix(df, word_dict):
+    """
+    Creates a matrix of unique combinations of words in dataframe of length unique words x numbers of tweets
+    """
     number_of_reviews = df.shape[0]
     number_of_words = len(word_dict)
     feature_matrix = np.zeros((number_of_reviews, number_of_words))
@@ -126,6 +133,9 @@ def generate_multi_class_feature_matrix(df, word_dict):
 
 
 def performance(y_true, Y_pred, metric="accuracy"):
+    """
+    Get the performance of the model results
+    """
     if metric == "accuracy":
         return metrics.accuracy_score(y_true, Y_pred)
     elif metric == "f1-score":
@@ -148,14 +158,15 @@ def performance(y_true, Y_pred, metric="accuracy"):
 
 
 def cv_performance(clf, X, y, k=5, metric="accuracy"):
+    """
+    Calculate prediction scores using K Fold cross validation
+    """
     skf = StratifiedKFold(n_splits=k)
     skf.get_n_splits(X,y)
     scores = []
     for train_index, test_index in skf.split(X, y):
         X_train, X_test = X[train_index], X[test_index]
         y_train, y_test = y[train_index], y[test_index]
-        #X_train, X_test = np.array(X)[train_index.astype(int)], np.array(X)[test_index.astype(int)]
-        #y_train, y_test = np.array(y)[train_index.astype(int)], np.array(y)[test_index.astype(int)]
         clf.fit(X_train, y_train)
         if metric == "auroc":
             pred_values = clf.decision_function(X_test)
@@ -170,7 +181,9 @@ def cv_performance(clf, X, y, k=5, metric="accuracy"):
 def select_param_linear(
     X, y, k=5, metric="accuracy", C_range=[], loss="hinge", penalty="l2", dual=True
 ):
-
+    """
+    Get the best regularization parameter for cross validation and the best cross validation score
+    """
     maxCV = 0
     parameter_value = 0
     for c in C_range:
@@ -186,6 +199,10 @@ def select_param_linear(
 
 
 def plot_weight(X, y, penalty, C_range, loss, dual):
+    """
+    Plot the different regularization parameters results for Support Vector Machines
+    """
+    norm0 = []
     for c in C_range:
         clf = LinearSVC(penalty=penalty, C=c, dual=dual, loss=loss)
         clf.fit(X, y)
@@ -205,6 +222,9 @@ def plot_weight(X, y, penalty, C_range, loss, dual):
 
 
 def select_param_quadratic(X, y, k=10, metric="accuracy", param_range=[]):
+    """
+    Get the optimal results for a Quadratic Support Vector Machines
+    """
     best_C_val, best_r_val = 0.0, 0.0
     parameter_value = 0
     for c,r in param_range:
@@ -217,7 +237,9 @@ def select_param_quadratic(X, y, k=10, metric="accuracy", param_range=[]):
     return best_C_val, best_r_val
 
 def get_average_non_zeroes(X_train):
-    # function that return the average non_zeroes per row
+    """
+    Gets the average non zeroes per row
+    """
     average_zeroes = 0
     for i in X_train:
         non_zero_counter = 0
@@ -229,12 +251,17 @@ def get_average_non_zeroes(X_train):
     return total
 
 def get_common_element(X_train, word_dict):
-    # function that returns the most common word
+    """
+    Get the most common word in the Matrix
+    """
     sum = np.sum(X_train, axis=0)
     result = np.where(sum == np.amax(sum, axis=0))
     return list(word_dict.keys())[list(word_dict.values()).index(result[0])]
 
 def one_vs_all_implementation(X_train, Y_train, X_test, Y_test):
+    """
+    Implementations the One-vs-all strategy which handles multiclass classification issues by training each class against the all the other classes
+    """
     Y_train_first = Y_train
     Y_test_first = Y_test
     Y_train_first[Y_train_first == 0] = -1
@@ -261,10 +288,8 @@ def one_vs_all_implementation(X_train, Y_train, X_test, Y_test):
         if metric != 'auroc':
             print(f'Test Performance on C_value: 1, {metric}', performance(Y_test_second, pred_values, metric=metric))
 
-    #Y_train_third = Y_train
-    #Y_test_third = Y_test
-    #Y_train_third[Y_train_third == -1] = 1
-    #Y_test_third[Y_test_third == -1] = 1
+    Y_train_third = Y_train
+    Y_test_third = Y_test
 
     clf = LinearSVC(penalty="l2", C=1, dual=True, loss="hinge")
     clf.fit(X_train, Y_train_third)
@@ -273,36 +298,36 @@ def one_vs_all_implementation(X_train, Y_train, X_test, Y_test):
         if metric != 'auroc':
             print(f'Test Performance on C_value: 1, {metric}', performance(Y_test_third, pred_values, metric=metric))
 
-
-
-
 def main():
-    # Read binary data
-    # NOTE: READING IN THE DATA WILL NOT WORK UNTIL YOU HAVE FINISHED
-    #       IMPLEMENTING generate_feature_matrix AND extract_dictionary
-
+    """
+    Program Flow
+    """
+    
     X_train, Y_train, X_test, Y_test, dictionary_binary = get_split_binary_data(
         fname="data/dataset.csv"
     )
     # 3a
-    #print("The processed sentence is", extract_word('BEST book ever! It\'s great'))
+    print("Question 3A")
+    print("The processed sentence is", extract_word('BEST book ever! It\'s great'))
 
     # 3b
-    #print("d: ",len(X_train[0]))
+    print("Question 3B")
+    print("d: ",len(X_train[0]))
     #3c
-    #print("Average number of nonzero features:", get_average_zeroes(X_train))
-    #print("Most common word:", get_common_element(X_train, dictionary_binary))
+    print("Question 3C")
+    print("Average number of nonzero features:", get_average_non_zeroes(X_train))
+    print("Most common word:", get_common_element(X_train, dictionary_binary))
+
     # 4.1
-    #4b
-    """
+    print("Question 4.1A")
     vecMetric = ["accuracy", "f1-score", "auroc", "precision", "sensitivity", "specificity"]
     for metric in vecMetric:
         select_param_linear(X_train, Y_train, k=5, metric=metric,
                             C_range=[.001, .01, .1, 1, 10, 100, 1000],
                             loss="hinge", penalty="l2", dual=True)
-    """
+    
     #4c
-    """
+    print("Question 4.1C")
     clf = LinearSVC(penalty="l2", C=0.01, dual=True, loss="hinge", random_state=445)
     clf.fit(X_train, Y_train)
     pred_auroc = clf.decision_function(X_test)
@@ -312,12 +337,13 @@ def main():
             print(f'{metric}:', performance(Y_test, pred_auroc, metric=metric))
         else:
             print(f'{metric}:', performance(Y_test, pred_values, metric=metric))
-    """
+    
     # 4d
-    #plot_weight(X_train, Y_train, penalty="l2", C_range=[.001, .01, .1, 1, 10, 100], loss="hinge", dual=True)
+    print("Question 4.1D")
+    plot_weight(X_train, Y_train, penalty="l2", C_range=[.001, .01, .1, 1, 10, 100], loss="hinge", dual=True)
 
     # 4e
-    """
+    print("Question 4.1E")
     clf = LinearSVC(penalty="l2", C=0.1, dual=True, loss="hinge", random_state=445)
     clf.fit(X_train, Y_train)
     max_values = np.argpartition(clf.coef_[0], -5)[-5:]
@@ -348,19 +374,21 @@ def main():
     plt.xlabel('Word Name')
     plt.ylabel('Word Coefficient')
     plt.show()
-    """
+    
 
     # 4.2
     # a
+    print("Question 4.2A")
     select_param_linear(X_train, Y_train, k=5, metric="accuracy",
                         C_range=[.001, .01, .1, 1], loss="squared_hinge",
                         penalty="l1", dual=False)
 
     # b
-    # plot_weight(X_train, Y_train, penalty="l1", C_range=[.001, .01, .1, 1], loss="squared_hinge", dual=False)
+    print("Question 4.2B")
+    plot_weight(X_train, Y_train, penalty="l1", C_range=[.001, .01, .1, 1], loss="squared_hinge", dual=False)
 
     # 4.3(b)i
-    """
+    print("Question 4.3B")
     c_params = [.001, .01, .1, 1, 10, 100, 1000]
     r_params = [.001, .01, .1, 1, 10, 100, 1000]
     i = 0
@@ -381,6 +409,7 @@ def main():
     print("Test Performance: ", performance(Y_test, pred_auroc, metric="auroc"))
 
     # 4.3 (b)ii
+    print("Question 4.3B(ii)")
     param_range = []
     i = 0
     j = 0
@@ -400,11 +429,10 @@ def main():
     print("Best c: ", best_c, end= " ")
     print("Best coeff: ", best_r)
     print("Test Performance: ", performance(Y_test, pred_auroc, metric="auroc"))
-    """
+    
 
     # 5.1
     # c
-    """
     print("Question 5.1: Linear SVM with imbalanced class weights")
     clf = LinearSVC(penalty="l2", C=0.01, loss="hinge", class_weight ={-1: 1, 1: 10}, random_state = 445)
     clf.fit(X_train, Y_train)
@@ -415,11 +443,11 @@ def main():
             print(f'Test Performance on {metric}:', performance(Y_test, pred_auroc, metric=metric))
         else:
             print(f'Test Performance on {metric}:', performance(Y_test, pred_values, metric=metric))
-    """
+    
 
     # 5.2
     # a
-    """
+    print("Question 5.2")
     IMB_features, IMB_labels, IMB_test_features, IMB_test_labels = get_imbalanced_data(
         dictionary_binary, fname="data/dataset.csv"
     )
@@ -433,10 +461,10 @@ def main():
             print(f'Test Performance on {metric}:', performance(IMB_test_labels, pred_auroc, metric=metric))
         else:
             print(f'Test Performance on {metric}:', performance(IMB_test_labels, pred_values, metric=metric))
-    """
+    
 
     # 5.3
-    """
+    
     print("Question 5.3(b): Choosing appropriate class weights")
     weight_values = [.1, .2, .3, .4, .5, .6, .7, .8, .9, 1]
     for weight in weight_values:
@@ -454,10 +482,10 @@ def main():
             print(f'Test Performance on {metric}:', performance(IMB_test_labels, pred_auroc, metric=metric))
         else:
             print(f'Test Performance on {metric}:', performance(IMB_test_labels, pred_values, metric=metric))
-    """
+    
     # 5.4
     # roc curve
-    """
+    print("Question 5.4: ROC Curve")
     clf_even = LinearSVC(penalty="l2", C=0.01, loss="hinge", class_weight ={-1: 1, 1: 1}, random_state=445)
     clf_even.fit(IMB_features, IMB_labels)
     pred_auroc_even = clf_even.decision_function(IMB_test_features)
@@ -476,20 +504,19 @@ def main():
     plt.title('ROC curve of evenly and unevenly weighted classes')
     plt.legend(loc="lower right")
     plt.show()
-    """
-    # Read multiclass data
+    
 
-    # TODO: Question 6: Apply a classifier to heldout features, and then use
-    #       generate_challenge_labels to print the predicted labels
+    # Question 6: Apply a classifier to heldout features, and then use generate_challenge_labels to print the predicted labels
+    print("Question 6")
     (
         X_train, Y_train, X_test, Y_test, dictionary
     ) = get_test_multiclass_training_data()
-    print("entered")
-    """
+
+    
     select_param_linear(X_train, Y_train, k=5, metric="accuracy",
                         C_range=[.001, .01, .1, 0.5, 0.8, 1, 1.2, 1.5, 2, 3], loss="hinge",
                         penalty="l2", dual=True)
-                        """
+                        
 
     clf = OneVsOneClassifier(LinearSVC(penalty="l2", C=0.1, loss="hinge", random_state=445))
     clf.fit(X_train, Y_train)
@@ -534,7 +561,7 @@ def main():
 
     generate_challenge_labels(pred_values, "test")
 
-    #print(f'Test Performance on accuracy:', performance(Y_test, pred_values, metric="accuracy"))
+    print(f'Test Performance on accuracy:', performance(Y_test, pred_values, metric="accuracy"))
 
     (
         multiclass_features,
@@ -544,12 +571,11 @@ def main():
 
     heldout_features = get_heldout_reviews(multiclass_dictionary)
     clf = OneVsRestClassifier(LinearSVC(penalty="l2", C=1, dual=True, loss="hinge"))
-    #clf = LinearSVC(penalty="l2", C=1, dual=True, loss="hinge")
     clf.fit(multiclass_features, multiclass_labels)
     pred_values = clf.predict(heldout_features)
 
 
-    #generate_challenge_labels(pred_values, "test")
+    generate_challenge_labels(pred_values, "test")
 
     C_range=[.001, .01, .1, 1, 10, 100, 1000]
 
@@ -557,15 +583,15 @@ def main():
        clf = OneVsOneClassifier(LinearSVC(penalty="l2", C=c, dual=True, loss="hinge", random_state=445))
        clf.fit(X_train, Y_train)
        pred_values = clf.predict(X_test)
-       # print(f'Test Performance on C_value: {c}, accuracy', performance(Y_test, pred_values, metric="accuracy"))
+       print(f'Test Performance on C_value: {c}, accuracy', performance(Y_test, pred_values, metric="accuracy"))
 
-    # heldout_features = get_heldout_reviews(multiclass_dictionary)
+    heldout_features = get_heldout_reviews(multiclass_dictionary)
     C_range=[.001, .01, .1, 1, 10, 100, 1000]
-    #for c in C_range:
-    #    clf = LinearSVC(penalty="l2", C=c, dual=True, loss="hinge")
-    #    clf.fit(multiclass_features, multiclass_labels)
-    #    pred_values = clf.predict(heldout_features)
-    #    print(f'Test Performance on C_value: {c} ', performance(Y_test, pred_values, metric="accuracy"))
+    for c in C_range:
+        clf = LinearSVC(penalty="l2", C=c, dual=True, loss="hinge")
+        clf.fit(multiclass_features, multiclass_labels)
+        pred_values = clf.predict(heldout_features)
+        print(f'Test Performance on C_value: {c} ', performance(Y_test, pred_values, metric="accuracy"))
 
     (
         multiclass_features,
